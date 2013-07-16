fe159456d5e1c11f73d1e3df1acb63dde18a63c5
cesar.reyes <cesar.reyes@upandrunningsoftware.com>
2013-07-16 15:32:16 -0500

    Instalacion basica de CI

---
+++ application/views/public/dashboard.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ 
+	<title><?= $this->config->item('app_name') ?></title>
+
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css" />
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script> 
+ 
+</head>
+ 
+<body>
+
+
+    <footer data-role="footer">
+        La forma mas fácil de pedir un taxi - www.pidetutaxi.com
+    </footer>
+</body>
+</html>
